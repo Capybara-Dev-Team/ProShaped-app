22677f83c0ffc9c3f29c4736a18054b15a1ff3c3
cosmin-oros <oroscosmin09@gmail.com>
2022-06-20 16:41:32 +0300

    bottomNav

--- ProShaped/app/src/main/java/com/example/proshapedapp/BottomNavItem.kt
+++ ProShaped/app/src/main/java/com/example/proshapedapp/BottomNavItem.kt
@@ -2,6 +2,7 @@ package com.example.proshapedapp
 
 import androidx.compose.ui.graphics.vector.ImageVector
 
+
 data class BottomNavItem(
     val name: String,
     val route: String,
--- ProShaped/app/src/main/java/com/example/proshapedapp/MainActivity.kt
+++ ProShaped/app/src/main/java/com/example/proshapedapp/MainActivity.kt
@@ -1,22 +1,48 @@
 package com.example.proshapedapp
 
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.proshapedapp.ui.theme.ProShapedAppTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.bottomnavigation.ui.theme.BottomNavigationTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BottomNavigationTheme {
+            ProShapedAppTheme {
                 val navController = rememberNavController()
                 Scaffold(
                     bottomBar = {
