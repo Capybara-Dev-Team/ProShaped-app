1cf65b60d6fe54cb7aa7b1fb738d42ec7804c160
cosmin-oros <oroscosmin09@gmail.com>
2022-06-27 18:15:08 +0300

    Updated settings screen

--- ProShaped/app/src/main/java/com/example/proshapedapp/caloriesScreenPackage/ListOfPhotos.kt
+++ ProShaped/app/src/main/java/com/example/proshapedapp/caloriesScreenPackage/ListOfPhotos.kt
@@ -25,6 +25,46 @@ object PhotosData{
             PhotoData(
                 calories = "1 cup(81g) Oats -> 307kcal",
                 imageResourceId = R.drawable.oats
+            ),
+            PhotoData(
+                calories = "100 grams of Beef -> 332kcal",
+                imageResourceId = R.drawable.beef
+            ),
+            PhotoData(
+                calories = "100 grams of Blueberry -> 57kcal",
+                imageResourceId = R.drawable.blueberry
+            ),
+            PhotoData(
+                calories = "3 boiled Eggs -> 155kcal",
+                imageResourceId = R.drawable.boiledeggs
+            ),
+            PhotoData(
+                calories = "100 grams of Brocolli -> 34kcal",
+                imageResourceId = R.drawable.brocolli
+            ),
+            PhotoData(
+                calories = "100 grams of Mexican Mix -> 92kcal",
+                imageResourceId = R.drawable.mexicanmix
+            ),
+            PhotoData(
+                calories = "100 grams of Pancakes -> 227kcal",
+                imageResourceId = R.drawable.pancakes
+            ),
+            PhotoData(
+                calories = "100 grams of Pasta -> 131kcal",
+                imageResourceId = R.drawable.pasta
+            ),
+            PhotoData(
+                calories = "100 grams of White Rice -> 130kcal",
+                imageResourceId = R.drawable.rice
+            ),
+            PhotoData(
+                calories = "100 grams of Salmon -> 208kcal",
+                imageResourceId = R.drawable.somon
+            ),
+            PhotoData(
+                calories = "100 grams of Strawberry -> 32kcal",
+                imageResourceId = R.drawable.strawberry
             )
             //to add more add photo to res/drawable and then create a photoData like above
         )
